bae79ee29ec4d63bfb46116891a41cba85dc94bf
youmna <youmna_salah@icloud.com>
2017-04-20 03:33:32 +0200

    assignment 2  starter code

---
+++ Go/src/AP.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+public class AP {
+
+	public static void main(String[] args) {
+
+		String inFile = "Go/MS2/Go1.in";
+		String outFile = "Go/MS2/Go11.out";
+
+		if (args.length > 1) {
+			inFile = args[0];
+		}
+
+		try {
+			FileReader fis = new FileReader(inFile);
+			BufferedReader bis = new BufferedReader(fis);
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+
+			@SuppressWarnings("deprecation")
+			parser parser = new parser(new Lexer(bis));
+
+			try {
+				parser.parse();
+				writer.write("Done");
+			} catch (Exception e) {
+				writer.write("ParseError");
+			}
+			
+			fis.close();
+			bis.close();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
